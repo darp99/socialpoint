98afaa5663b49f50182bbb2c61c8150c0009ca03
krishnan-tech <krishnannavadia@gmail.com>
2021-10-07 17:51:52 +0530

    added cors and fix some bugs

--- client/src/components/screens/Newpassword.js
+++ client/src/components/screens/Newpassword.js
@@ -1,57 +1,55 @@
-import React,{useState,useContext,} from 'react'
-import {Link,useHistory,useParams} from 'react-router-dom'
-import M from 'materialize-css'
-const SignIn  = ()=>{
-    const history = useHistory()
-    const [password,setPasword] = useState("")
-    const {token} = useParams()
-    console.log(token)
-    const PostData = ()=>{
-        fetch("/new-password",{
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({
-                password,
-                token
-            })
-        }).then(res=>res.json())
-        .then(data=>{
-            console.log(data)
-           if(data.error){
-              M.toast({html: data.error,classes:"#c62828 red darken-3"})
-           }
-           else{
+import React, { useState, useContext } from "react";
+import { Link, useHistory, useParams, Redirect } from "react-router-dom";
+import M from "materialize-css";
+const SignIn = () => {
+  const [password, setPasword] = useState("");
+  const { token } = useParams();
 
-               M.toast({html:data.message,classes:"#43a047 green darken-1"})
-               history.push('/signin')
-           }
-        }).catch(err=>{
-            console.log(err)
-        })
-    }
-   return (
-      <div className="mycard">
-          <div className="card auth-card input-field">
-            <h2>Socialpoint</h2>
-        
-            <input
-            type="password"
-            placeholder="enter a new password"
-            value={password}
-            onChange={(e)=>setPasword(e.target.value)}
-            />
-            <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
-            onClick={()=>PostData()}
-            >
-               Update password
-            </button>
-    
-        </div>
+  const PostData = () => {
+    fetch("/new-password", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password,
+        token,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          M.toast({ html: data.error, classes: "#c62828 red darken-3" });
+        } else {
+          M.toast({ html: data.message, classes: "#43a047 green darken-1" });
+          <Redirect to="/signin" />;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Socialpoint</h2>
+
+        <input
+          type="password"
+          placeholder="enter a new password"
+          value={password}
+          onChange={(e) => setPasword(e.target.value)}
+        />
+        <button
+          className="btn waves-effect waves-light #64b5f6 blue darken-1"
+          onClick={() => PostData()}
+        >
+          Update password
+        </button>
       </div>
-   )
-}
+    </div>
+  );
+};
 
-
-export default SignIn
+export default SignIn;
